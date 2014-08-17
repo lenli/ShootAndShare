0d9202a1277bcbba8412f6b6c1481009ba17e9f6
Len Li <leonard.li@gmail.com>
2014-08-17 14:38:18 -0400

    Add video preview

--- ShootAndShare/SSVideoRecorderViewController.h
+++ ShootAndShare/SSVideoRecorderViewController.h
@@ -8,6 +8,8 @@
 
 #import <UIKit/UIKit.h>
 #import <AVFoundation/AVFoundation.h>
+//#import <QuartzCore/QuartzCore.h>
+//#import <CoreMedia/CoreMedia.h>
 
 @interface SSVideoRecorderViewController : UIViewController <AVCaptureVideoDataOutputSampleBufferDelegate>
 
--- ShootAndShare/SSVideoRecorderViewController.m
+++ ShootAndShare/SSVideoRecorderViewController.m
@@ -9,6 +9,7 @@
 #import "SSVideoRecorderViewController.h"
 
 @interface SSVideoRecorderViewController ()
+@property (weak, nonatomic) IBOutlet UIView *cameraPreviewView;
 
 @end
 
@@ -16,12 +17,34 @@
 
 - (void)viewDidLoad
 {
+    NSLog(@"view did load");
     [super viewDidLoad];
     [self setupCamera];
 }
 
 - (void)setupCamera {
+    NSLog(@"Setup Camera");
+    // Session
+    AVCaptureSession *session = [AVCaptureSession new];
+    [session setSessionPreset:AVCaptureSessionPresetMedium];
     
+    // Capture device
+    AVCaptureDevice *inputDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    NSError *error;
+    
+    // Capture Device Input
+    AVCaptureDeviceInput *deviceInput = [AVCaptureDeviceInput deviceInputWithDevice:inputDevice error:&error];
+	if ( [session canAddInput:deviceInput] )
+        [session addInput:deviceInput];
+    
+    // Preview
+    AVCaptureVideoPreviewLayer *previewLayer = [AVCaptureVideoPreviewLayer layerWithSession:session];
+    previewLayer.frame = self.cameraPreviewView.bounds;
+    [previewLayer setVideoGravity:AVLayerVideoGravityResize];
+    
+    [self.cameraPreviewView.layer addSublayer:previewLayer];
+    
+    [session startRunning];
 }
 
 @end
